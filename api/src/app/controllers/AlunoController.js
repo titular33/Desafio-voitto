5fa6b9d6736cda87bb744486bf37fcdd4b4d6ea8
amorim.pedro@engenharia.ufjf.br <amorim.pedro@engenharia.ufjf.br>
2022-05-11 19:36:03 -0300

    Teste de logica e Crud

--- api/src/app/controllers/AlunoController.js
+++ api/src/app/controllers/AlunoController.js
@@ -7,20 +7,68 @@ class AlunoController {
   }
 
   async read(req, res) {
-    // TODO
+    try {
+      const{id} = req.params;
+      if(!id) {
+        return res.status(400).json({error: ['Id não informado'],});
+      }
+ const aluno = await Aluno.findByPk(id);
+  if(!aluno) {
+    return res.status(400).json({error: ['Aluno não encontrado'],});
   }
 
+  return res.json(aluno);
+
+  } 
+  catch (e)
+   {
+    return res.status(400).json({error: e.errors.map(err => err.message)});
+  }
+  }
   async create(req, res) {
-    // TODO
-  }
+    try {
+      const aluno = await Aluno.create(req.body);
+  return res.json(aluno);
+  } catch (e) {
+    return res.status(400).json({error: e.errors.map(err => err.message)});
 
+  }
+  }
   async update(req, res) {
-    // TODO
+    try {
+      const{id} = req.params;
+      if(!id) {
+        return res.status(400).json({error: ['Id não informado'],});
+      }
+ const aluno = await Aluno.findByPk(id);
+  if(!aluno) {
+    return res.status(400).json({error: ['Aluno não encontrado'],});
   }
+  const alunoAtualizado = aluno.uptade(req.body);
+  return res.json(alunoAtualizado);
+  } 
+  catch (e) {
+    return res.status(400).json({error: e.errors.map(err => err.message)});
 
+  }
+  }
   async delete(req, res) {
-    // TODO
+    try {
+      const{id} = req.params;
+      if(!id) {
+        return res.status(400).json({error: ['Id não informado'],});
+      }
+ const aluno = await Aluno.findByPk(id);
+  if(!aluno) {
+    return res.status(400).json({error: ['Aluno não encontrado'],});
+  }
+  await aluno.destroy();
+  return res.json({apagado: true,});
+  } 
+  catch (e) {
+    return res.status(400).json({error: e.errors.map(err => err.message)});
+
+  }
   }
 }
-
 export default new AlunoController();
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,5 +5,10 @@ import AlunosController from '../app/controllers/AlunoController';
 const routes = new Router();
 
 routes.get('/alunos', AlunosController.index);
+routes.post('/alunos', AlunosController.store);
+routes.put('/alunos/:id', AlunosController.update);
+routes.get('/alunos/:id', AlunosController.show);
+routes.delete('/alunos/:id', AlunosController.delete);  
+
 
 export default routes;
